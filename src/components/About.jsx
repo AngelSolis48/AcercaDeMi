26e9e28fdc65881ff961ea56f7eed6ce3bb40655
AngelSolis48 <tobii48@gmail.com>
2021-07-31 18:41:22 -0500

    Estilos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,11 @@ import styled from "styled-components";
 
 const AboutStyle = styled.div`
     text-align: center;
+    float: left;
+    margin: 0 1em 0 0;
+    border-radius: 15px;
+    box-shadow: 0 0 .5em rgba(0,0,0,0.5);
+    min-width: 450px;
 `;
 
 const AboutAvatar = styled.div`
@@ -15,7 +20,35 @@ const AboutImg = styled.img`
     width: 180px;
     height: 180px;
     box-shadow: 0 0 10px rgba(0,0,0,0.9);
+    border: 6px solid #1e6197;
 `;
+
+const AboutName = styled.div`
+    text-align: center;
+`;
+
+const AboutH2 = styled.h2`
+    font-family: 'Montserrat',sans-serif;
+    font-weight: 700;
+    letter-spacing: -1.2px;
+    margin: 5em 0 0 0;
+    color: #1b1725;
+`;
+
+const AboutProfession = styled.p`
+    font-family: 'Montserrat', sans-serif;
+    margin: 0 0 1em 0;
+    letter-spacing: 1.6;
+    font-weight: 700;
+    color: #1b1725;
+`;
+
+const AboutBio = styled.p`
+    color: #1b1725;
+    font-weight: 400;
+    font-size: 1em;
+`;
+
 const About = ({ avatar, name, profession, bio, address, social }) => {
   return (
     <AboutStyle>
@@ -25,14 +58,14 @@ const About = ({ avatar, name, profession, bio, address, social }) => {
             <AboutImg src={avatar} alt={name} />
           </figure>
         </AboutAvatar>
-        <div className="About-name">
-          <h2>{name}</h2>
-        </div>
+        <AboutName>
+          <AboutH2>{name}</AboutH2>
+        </AboutName>
         <div className="About-profession">
-          <p>{profession}</p>
+          <AboutProfession>{profession}</AboutProfession>
         </div>
         <div className="About-desc">
-          <p>{bio}</p>
+          <AboutBio>{bio}</AboutBio>
         </div>
         <div className="About-location">
           <p>{address}</p>
